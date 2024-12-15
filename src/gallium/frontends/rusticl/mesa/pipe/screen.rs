7b615daeb95af2daaf22e69a68b72e2cdac5cf08
Karol Herbst <kherbst@redhat.com>
2024-12-16 00:34:23 +0100

    rusticl/mesa: wrap new VM interfaces SVM edition
    Reviewed-by: Adam Jackson <ajax@redhat.com>

--- src/gallium/frontends/rusticl/mesa/pipe/screen.rs
+++ src/gallium/frontends/rusticl/mesa/pipe/screen.rs
@@ -9,6 +9,7 @@ use mesa_rust_util::has_required_feature;
 use mesa_rust_util::ptr::ThreadSafeCPtr;
 
 use std::ffi::CStr;
+use std::num::NonZeroU64;
 use std::os::raw::c_schar;
 use std::os::raw::c_uchar;
 use std::os::raw::c_void;
@@ -40,6 +41,18 @@ impl ResourceType {
             }
             Self::Normal => {}
         }
+    }
+}
+
+pub struct ScreenVMAllocation<'a> {
+    screen: &'a PipeScreen,
+    start: NonZeroU64,
+    size: NonZeroU64,
+}
+
+impl Drop for ScreenVMAllocation<'_> {
+    fn drop(&mut self) {
+        self.screen.free_vm(self.start, self.size);
     }
 }
 
@@ -77,6 +90,32 @@ impl PipeScreen {
             },
             self,
         )
+    }
+
+    pub fn alloc_vm(&self, start: NonZeroU64, size: NonZeroU64) -> Option<ScreenVMAllocation> {
+        unsafe { self.screen().alloc_vm?(self.screen.as_ptr(), start.get(), size.get()) }.then(
+            || ScreenVMAllocation {
+                screen: self,
+                start: start,
+                size: size,
+            },
+        )
+    }
+
+    fn free_vm(&self, start: NonZeroU64, size: NonZeroU64) {
+        if let Some(free_vm) = self.screen().free_vm {
+            unsafe {
+                free_vm(self.screen.as_ptr(), start.get(), size.get());
+            }
+        }
+    }
+
+    pub fn resource_assign_vma(&self, res: &PipeResource, address: u64) -> bool {
+        if let Some(resource_assign_vma) = self.screen().resource_assign_vma {
+            unsafe { resource_assign_vma(self.screen.as_ptr(), res.pipe(), address) }
+        } else {
+            false
+        }
     }
 
     fn resource_create(&self, tmpl: &pipe_resource) -> Option<PipeResource> {
@@ -354,6 +393,12 @@ impl PipeScreen {
 
     pub fn is_fixed_address_supported(&self) -> bool {
         self.screen().resource_get_address.is_some()
+    }
+
+    pub fn is_vm_supported(&self) -> bool {
+        self.screen().resource_assign_vma.is_some()
+            && self.screen().alloc_vm.is_some()
+            && self.screen().free_vm.is_some()
     }
 
     pub fn nir_shader_compiler_options(
