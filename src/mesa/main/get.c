alexvorxx/Mesa-VirGL

commit 328c29d6007ed7677d5c5307bd6997d84a32104a

mesa,glsl,gallium: add GL_OVR_multiview
Co-authored-by: Mike Blumenkrantz <michael.blumenkrantz@gmail.com>

Reviewed-by: Marek Olšák <marek.olsak@amd.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/31128>

src/mapi/glapi/gen/OVR_multiview.xml
@@ -1,0 +1,26 @@
+<?xml version="1.0"?>
+<!DOCTYPE OpenGLAPI SYSTEM "gl_API.dtd">
+
+<OpenGLAPI>
+
+<category name="GL_OVR_multiview">
+    <function name="FramebufferTextureMultiviewOVR" es2="3.0" desktop="true" no_error="true">
+        <param name="target" type="GLenum"/>
+        <param name="attachment" type="GLenum"/>
+        <param name="texture" type="GLuint"/>
+        <param name="level" type="GLint"/>
+        <param name="baseviewindex" type="GLint"/>
+        <param name="numviews" type="GLsizei"/>
+    </function>
+
+    <function name="NamedFramebufferTextureMultiviewOVR" es2="3.0" desktop="true" no_error="true">
+        <param name="framebuffer" type="GLuint"/>
+        <param name="attachment" type="GLenum"/>
+        <param name="texture" type="GLuint"/>
+        <param name="level" type="GLint"/>
+        <param name="baseviewindex" type="GLint"/>
+        <param name="numviews" type="GLsizei"/>
+    </function>
+</category>
+
+</OpenGLAPI>

src/compiler/glsl/ast.h
@@ -607,6 +607,12 @@ struct ast_type_qualifier {
          unsigned explicit_xfb_stride:1; /**< xfb_stride value assigned explicitly by shader code */
          /** \} */
 
+         /**
+          * Flag set if GL_OVR_multiview num_views_relative layout
+          * qualifier is used.
+          */
+         unsigned explicit_numviews:1;
+
 	 /** \name Layout qualifiers for GL_ARB_tessellation_shader */
 	 /** \{ */
 	 /* tess eval input layout */
@@ -744,6 +750,14 @@ struct ast_type_qualifier {
     * This field is only valid if \c explicit_binding is set.
     */
    ast_expression *binding;
+
+   /**
+    * Binding specified via GL_OVR_multiview's "num_views" keyword.
+    *
+    * \note
+    * This field is only valid if \c explicit_numviews is set.
+    */
+   ast_expression *num_views;
 
    /**
     * Offset specified via GL_ARB_shader_atomic_counter's or

src/compiler/glsl/ast_type.cpp
@@ -84,7 +84,8 @@ ast_type_qualifier::has_layout() const
           || this->flags.q.explicit_stream
           || this->flags.q.explicit_xfb_buffer
           || this->flags.q.explicit_xfb_offset
-          || this->flags.q.explicit_xfb_stride;
+          || this->flags.q.explicit_xfb_stride
+          || this->flags.q.explicit_numviews;
 }
 
 bool
@@ -186,6 +187,26 @@ validate_point_mode(ASSERTED const ast_type_qualifier &qualifier,
    return true;
 }
 
+static bool
+validate_view_qualifier(YYLTYPE *loc, struct _mesa_glsl_parse_state *state,
+                          unsigned view)
+{
+   if (view > MAX_VIEWS_OVR) {
+      _mesa_glsl_error(loc, state,
+                       "invalid view specified %d is larger than "
+                       "MAX_VIEWS_OVR (%d).",
+                       view, MAX_VIEWS_OVR);
+      return false;
+   }
+   else if (view <= 0) {
+      _mesa_glsl_error(loc, state,
+                       "invalid view specified %d is less than 0", view);
+      return false;
+   }
+
+   return true;
+}
+
 static void
 merge_bindless_qualifier(_mesa_glsl_parse_state *state)
 {
@@ -272,6 +293,7 @@ ast_type_qualifier::merge_qualifier(YYLTYPE *loc,
    input_layout_mask.flags.q.sample = 1;
    input_layout_mask.flags.q.smooth = 1;
    input_layout_mask.flags.q.non_coherent = 1;
+   input_layout_mask.flags.q.explicit_numviews = 1;
 
    if (state->has_bindless()) {
       /* Allow to use image qualifiers with shader inputs/outputs. */
@@ -497,6 +519,18 @@ ast_type_qualifier::merge_qualifier(YYLTYPE *loc,
       this->flags.q.out = 1;
    }
 
+   if (q.flags.q.explicit_numviews) {
+      this->num_views = q.num_views;
+      unsigned num_views;
+      if (process_qualifier_constant(state, loc, "num_views",
+                                     this->num_views, &num_views)){
+        if (!validate_view_qualifier(loc, state, num_views)){
+            _mesa_glsl_error(loc, state,
+                  "Invalid num_views specified");
+        }
+      }
+   }
+
    return r;
 }
 
@@ -603,6 +637,12 @@ ast_type_qualifier::validate_in_qualifier(YYLTYPE *loc,
    valid_in_mask.flags.i = 0;
 
    switch (state->stage) {
+   case MESA_SHADER_VERTEX:
+      if (this->flags.q.explicit_numviews){
+          valid_in_mask.flags.q.explicit_numviews = 1;
+          break;
+      }
+
    case MESA_SHADER_TESS_EVAL:
       if (this->flags.q.prim_type) {
          /* Make sure this is a valid input primitive type. */
@@ -708,6 +748,8 @@ ast_type_qualifier::merge_into_in_qualifier(YYLTYPE *loc,
       state->fs_early_fragment_tests = true;
       state->in_qualifier->flags.q.early_fragment_tests = false;
    }
+
+   state->in_qualifier->flags.q.explicit_numviews = false;
 
    if (state->in_qualifier->flags.q.inner_coverage) {
       state->fs_inner_coverage = true;
@@ -890,6 +932,7 @@ ast_type_qualifier::validate_flags(YYLTYPE *loc,
    Q2(explicit_xfb_buffer, xfb_buffer);
    Q2(xfb_stride, xfb_stride);
    Q2(explicit_xfb_stride, xfb_stride);
+   Q2(explicit_numviews, num_views);
    Q(vertex_spacing);
    Q(ordering);
    Q(point_mode);

src/compiler/glsl/builtin_variables.cpp
@@ -1144,6 +1144,10 @@ builtin_variable_generator::generate_vs_special_vars()
       add_system_value(SYSTEM_VALUE_VERTEX_ID, int_t, GLSL_PRECISION_HIGH,
                        "gl_VertexID");
    }
+   if (state->is_version(300, 300) && state->OVR_multiview_enable){
+      add_system_value(SYSTEM_VALUE_VIEW_INDEX, int_t, GLSL_PRECISION_MEDIUM,
+                      "gl_ViewID_OVR");
+   }
    if (state->is_version(460, 0)) {
       add_system_value(SYSTEM_VALUE_BASE_VERTEX, int_t, "gl_BaseVertex");
       add_system_value(SYSTEM_VALUE_BASE_INSTANCE, int_t, "gl_BaseInstance");

src/compiler/glsl/glsl_parser.yy
@@ -1765,6 +1765,11 @@ layout_qualifier_id:
          $$.location = $3;
       }
 
+      if (match_layout_qualifier("num_views", $1, state) == 0) {
+         $$.flags.q.explicit_numviews = 1;
+         $$.num_views = $3;
+      }
+
       if (match_layout_qualifier("component", $1, state) == 0) {
          if (!state->has_enhanced_layouts()) {
             _mesa_glsl_error(& @1, state,

src/compiler/glsl/glsl_parser_extras.cpp
@@ -846,6 +846,7 @@ static const _mesa_glsl_extension _mesa_glsl_supported_extensions[] = {
    EXT(NV_shader_atomic_int64),
    EXT(NV_shader_noperspective_interpolation),
    EXT(NV_viewport_array2),
+   EXT(OVR_multiview),
 };
 
 #undef EXT

src/compiler/glsl/glsl_parser_extras.h
@@ -934,6 +934,8 @@ struct _mesa_glsl_parse_state {
    bool NV_shader_noperspective_interpolation_warn;
    bool NV_viewport_array2_enable;
    bool NV_viewport_array2_warn;
+   bool OVR_multiview_enable;
+   bool OVR_multiview_warn;
    /*@}*/
 
    /** Extensions supported by the OpenGL implementation. */

src/gallium/auxiliary/util/u_framebuffer.c
@@ -106,6 +106,7 @@ util_copy_framebuffer_state(struct pipe_framebuffer_state *dst,
 
       dst->nr_cbufs = src->nr_cbufs;
 
+      dst->viewmask = src->viewmask;
       pipe_surface_reference(&dst->zsbuf, src->zsbuf);
       pipe_resource_reference(&dst->resolve, src->resolve);
    } else {
@@ -119,6 +120,8 @@ util_copy_framebuffer_state(struct pipe_framebuffer_state *dst,
          pipe_surface_reference(&dst->cbufs[i], NULL);
 
       dst->nr_cbufs = 0;
+
+      dst->viewmask = 0;
 
       pipe_surface_reference(&dst->zsbuf, NULL);
       pipe_resource_reference(&dst->resolve, NULL);
@@ -141,6 +144,8 @@ util_unreference_framebuffer_state(struct pipe_framebuffer_state *fb)
    fb->samples = fb->layers = 0;
    fb->width = fb->height = 0;
    fb->nr_cbufs = 0;
+
+   fb->viewmask = 0;
 }
 
 

KiritakeKumi/mesa-pvr-zink-24

commit 50289abbcd6589b2d9c259ef9346771eeb726bb7

mesa: Handle getting GL_MAX_VIEWS_OVR
Add support for GL_OVR_multiview's GL_MAX_VIEWS_OVR which can be
accessed with glGetIntegerv().

MaxViews is accessed via the hash table set up by get_hash_params.py as
a constant (MAX_VIEWS_OVR) using GL_MAX_VIEWS_OVR.

v2: Add this patch (thanks to Mike's guidance)
v3: Drop unnecessary enum size element in OVR_multiview.XML
v4: Switch to CONST(MAX_VIEWS_OVR) instead of gl_constants::MaxViews
    (Marek's suggestion)

Fixes: 328c29d6007 ("mesa,glsl,gallium: add GL_OVR_multiview")
Reviewed-By: Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
Signed-off-by: James Hogan <james@albanarts.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/32992>

src/mapi/glapi/gen/OVR_multiview.xml
@@ -21,6 +21,8 @@
         <param name="baseviewindex" type="GLint"/>
         <param name="numviews" type="GLsizei"/>
     </function>
+
+    <enum name="MAX_VIEWS_OVR" count="1" value="0x9631"/>
 </category>
 
 </OpenGLAPI>

src/mesa/main/get.c
@@ -592,6 +592,7 @@ EXTRA_EXT(ARB_spirv_extensions);
 EXTRA_EXT(NV_viewport_swizzle);
 EXTRA_EXT(ARB_sparse_texture);
 EXTRA_EXT(KHR_shader_subgroup);
+EXTRA_EXT(OVR_multiview);
 
 static const int extra_ARB_gl_spirv_or_es2_compat[] = {
    EXT(ARB_gl_spirv),

src/mesa/main/get_hash_params.py
@@ -498,6 +498,9 @@ descriptor=[
 # GL_ARB_cull_distance, GL_EXT_clip_cull_distance
   [ "MAX_CULL_DISTANCES", "CONTEXT_INT(Const.MaxClipPlanes), extra_ARB_cull_distance" ],
   [ "MAX_COMBINED_CLIP_AND_CULL_DISTANCES", "CONTEXT_INT(Const.MaxClipPlanes), extra_ARB_cull_distance" ],
+
+# OVR_multiview
+  [ "MAX_VIEWS_OVR", "CONST(MAX_VIEWS_OVR), extra_OVR_multiview" ],
 ]},
 
 { "apis": ["GLES", "GLES2"], "params": [